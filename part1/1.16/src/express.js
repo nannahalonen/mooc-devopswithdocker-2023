8390f28e3c2b0fb32cb4c11686b1ff8377410762
Nanna Halonen <nanna.halonen@gmail.com>
2023-04-01 13:59:36 +0300

    3.1 Activate watchtower updates

--- part1/1.16/src/express.js
+++ part1/1.16/src/express.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 8080
 
 app.get('/', (req, res) => {
-  res.send('Hello World from nannahalonen/mooc-devopswithdocker-2023 and docker hub #3!')
+  res.send('Hello World from nannahalonen/mooc-devopswithdocker-2023 and docker hub #4!')
 })
 
 app.listen(port, () => {
